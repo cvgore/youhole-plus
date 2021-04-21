f0f542621bd73c9d5b82c3fb12982a732511a3be
Kacper <kacper@cvgo.re>
2021-04-21 14:20:15 +0200

    Update youhole-plus.user.js

--- youhole-plus.user.js
+++ youhole-plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         You Hole Plus
 // @namespace    http://cvgo.re/
-// @version      0.1.0
+// @version      0.2.0
 // @description  try to take over the world!
 // @author       cvgore
 // @match        http://youhole.tv/
@@ -14,16 +14,87 @@
 (function() {
     "use strict";
 
-    const watchOnYtBtn = document.createElement("a");
+    const symbols = {
+        play: "⏵︎",
+        pause: "⏸︎",
+    };
 
-    watchOnYtBtn.innerHTML = "View on Youtube"
-    watchOnYtBtn.style.right = 0;
-    watchOnYtBtn.style.bottom = 0;
-    watchOnYtBtn.style.zIndex = 1000;
-    watchOnYtBtn.style.position = 'fixed';
-    watchOnYtBtn.style.padding = '.5rem 1rem';
-    watchOnYtBtn.style.backgroundColor = '#fff';
-    watchOnYtBtn.style.fontFamily = "sans-serif";
+    function store(obj) {
+        const watchers = {};
+
+        obj.listen = (prop, cb) => {
+            if (!watchers[prop]) {
+                watchers[prop] = [];
+            }
+
+            watchers[prop].push(cb);
+        };
+
+        const proxy = new Proxy(obj, {
+            set(target, prop, val) {
+                if (watchers[prop]) {
+                    watchers[prop].forEach(x => x(val));
+                }
+
+                return Reflect.set(...arguments);
+            }
+        });
+
+        return proxy;
+    }
+
+    const state = store({
+        playbackStatus: 'pause'
+    });
+
+    const containerEl = document.createElement("div");
+
+    containerEl.style.right = 0;
+    containerEl.style.bottom = 0;
+    containerEl.style.zIndex = 1000;
+    containerEl.style.position = 'fixed';
+
+    document.body.appendChild(containerEl);
+
+    function mkBtn(label) {
+        const btn = document.createElement("a");
+
+        btn.innerHTML = label;
+        btn.style.padding = ".5rem 1rem";
+        btn.style.backgroundColor = "#e6e6e6";
+        btn.style.textDecoration = "none";
+        btn.style.borderRadius = 2;
+        btn.style.fontFamily = "sans-serif";
+        btn.style.color = "rgba(0,0,0,.8)";
+        btn.style.marginLeft = ".128rem";
+
+        containerEl.appendChild(btn);
+
+        return btn;
+    }
+
+    const playPauseBtn = mkBtn(symbols.play);
+    playPauseBtn.addEventListener("click", (ev) => {
+        ev.preventDefault();
+
+        const playerEl = document.querySelector("#player");
+
+        playerEl.contentWindow.postMessage(
+            JSON.stringify({
+                event: "command",
+                func: state.playbackStatus === "pause" ? "playVideo" : "pauseVideo",
+                args: [],
+                id: playerEl.id
+            }),
+            '*'
+        );
+    }, false);
+
+    state.listen('playbackStatus', (val) => {
+        playPauseBtn.innerHTML = val === "pause" ? symbols.play : symbols.pause;
+    });
+
+    const watchOnYtBtn = mkBtn("View on Youtube");
     watchOnYtBtn.target = "_blank";
 
     window.onmessage = (msg) => {
@@ -41,7 +112,11 @@
            ) {
             watchOnYtBtn.href = `https://www.youtube.com/watch?v=${data.info.videoData.video_id}`;
         }
-    };
 
-    document.body.appendChild(watchOnYtBtn);
+        if (data.event === "onStateChange"
+            && data.id === 1
+            ) {
+            state.playbackStatus = data.info == 2 ? "pause" : "play";
+        }
+    };
 })();
